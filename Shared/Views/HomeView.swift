fbcca7fb1961a3133a334c29e92f27450b0dbb67
Developer <developer@ylabz.com>
2021-08-22 07:43:21 -0600

    minor comment delete

--- Shared/Views/HomeView.swift
+++ Shared/Views/HomeView.swift
@@ -68,51 +68,12 @@ struct HomeView: View {
                     Text("Combine")
                 }.badge(7)
         }
-        /*NavigationView {
-         VStack {
-         GikeGridView()
-         NavigationLink(destination: BikeStationView()) {
-         Text("Bike Stations")
-         }.navigationBarTitle("Bike Stations")
-         /*NavigationLink(destination: HomeView()) {
-          Text("Home")
-          }.navigationBarTitle("Bike Home")*/
-         }
-         }
-         }*/
-        /* VStack{
-         Text("Lets Get Some Bikes !!!")
-         BikeMenu()
-         FormView()
-         }*/
     }
     
     func skip() {
         print("Menu Pressed")
     }
     
-    /*
-     struct BikeMenu: View {
-     var body: some View {
-     Menu("Actions") {
-     Button("Duplicate", action: {skip()})
-     Button("Rename", action: {skip()})
-     Button("Delete…", action: {skip()})
-     Menu("Bike Info") {
-     Button("Copy", action: {skip()})
-     Button("Copy Formatted", action: {skip()})
-     Button("Copy Library Path", action: {skip()})
-     NavigationLink(destination: BikeStationView()) {
-     Text("Bike System Info")
-     }.navigationBarTitle("Bike Menu")
-     NavigationLink(destination: BikeStationView()) {
-     Text("Bike Stations")
-     }.navigationBarTitle("Bike Stations")
-     }
-     }
-     }
-     }
-     */
     
     struct HomeView_Previews: PreviewProvider {
         static var previews: some View {
